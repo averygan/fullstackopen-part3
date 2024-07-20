eb53d70aa017a31ad39092d833cd2e8296ac9153
averygan <averygan19@gmail.com>
2024-07-20 17:15:07 +0800

    completed ex 3.21 - added validator to schema and deployed to production

--- index.js
+++ index.js
@@ -60,19 +60,8 @@ const generateId = () => {
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postrequest'))
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
-  if (!body.name || !body.number) {
-    return response.status(400).json({
-      error: 'name and/or number missing'
-    })
-  }
-
-  if (isNaN(body.number)) {
-    return response.status(400).json({
-      error: 'number must be a valid number'
-    })
-  }
 
   const person = new Person({
     name: body.name,
@@ -95,10 +84,11 @@ app.put('/api/persons/:id', (request, response, next) => {
     number: body.number,
   }
 
-  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+  Person.findByIdAndUpdate(request.params.id, person, { new: true, runValidators: true })
     .then(updatedPerson => {
       response.json(updatedPerson)
     })
+    .catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
@@ -113,7 +103,10 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
-  } 
+  }
+  else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
   next(error)
 }
 
